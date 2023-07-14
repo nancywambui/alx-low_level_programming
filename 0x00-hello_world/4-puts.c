26607f4b85ac36f9dabf6a10ad503c12e30ba74c
nancywambui <nwambuimuthee@gmail.com>
2023-07-14 12:49:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio>
+/**
+ * main - a program that anyone can understand
+ * Return: 0 (success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
