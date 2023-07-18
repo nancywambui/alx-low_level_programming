b024c5b84b0eec5697242614e9b7fe01e802b84a
nancywambui <nwambuimuthee@gmail.com>
2023-07-18 14:05:11 -0700

    0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,7 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
 
-int main() 
+int main(void)
 {
+
 printf("_putchar\n");
-return 0;
+
+return (0);
 }
+
