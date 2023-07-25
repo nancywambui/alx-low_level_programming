7df56914ab67f05e042b37d2db9961559ccd050d
nancywambui <nwambuimuthee@gmail.com>
2023-07-25 03:39:19 -0700

    main.h

---
+++ 0-reset_to_98.c/main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+// Prototype of the function _putchar (assuming it's used for printing)
+int _putchar(char c);
+
+// Prototypes of your functions
+void reset_to_98(int *n);
+// Add other function prototypes here if needed
+
+#endif /* MAIN_H */
