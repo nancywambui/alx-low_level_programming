25fdccffcdced5c9fbe661bcf6b2c410626e48db
nancywambui <nwambuimuthee@gmail.com>
2023-08-01 03:58:18 -0700

    0-memset.c

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _memset - fills the first bytes of the memory area
+ * @s: pointer to the memory area where the constant byte is written
+ * @b: its the constant byte to be written
+ * @n: the number of bytes to be filled
+ *
+ * Return: it returns a pointer to the memory  area
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	for (unsigned int i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	reurn(s);
+}
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+
+/**
+ *  _purchar - we are using it to write the character of c to stdout
+ *  @c: this is the character that we need to print
+ *
+ *  Return: 1 success
+ *  On error, -1 returned, then error is set well.
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
