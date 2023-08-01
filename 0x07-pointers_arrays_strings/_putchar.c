874c307f5f1d66c8b08e56c396ceb570e86411df
nancywambui <nwambuimuthee@gmail.com>
2023-08-01 04:03:21 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/_putchar.c
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -2,13 +2,13 @@
 #include <unistd.h>
 
 /**
- *  _purchar - we are using it to write the character of c to stdout
- *  @c: this is the character that we need to print
+ *_purchar - we are using it to write the character of c to stdout
+ *@c: this is the character that we need to print
  *
- *  Return: 1 success
- *  On error, -1 returned, then error is set well.
+ *Return: 1 success
+ *On error, -1 returned, then error is set well.
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+return (write(1, &c, 1));
 }
