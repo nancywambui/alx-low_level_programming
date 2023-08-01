4059e94aa70dafd9b39a6ac8f97abc4a0948e9ca
nancywambui <nwambuimuthee@gmail.com>
2023-08-01 06:30:01 -0700

    5-strstr.c

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _strstr - function
+ * @needle: the pointer to the string
+ * @haystack: the pointer to the string of the character
+ * Return: 0 success
+ */
+char *_strstr(char *haystack, char *needle)
+{
+char *l = haystack;
+char *p = needle;
+
+while (*l == *p && *p != '\0')
+{
+l++;
+p++;
+}
+
+if (*p == '\0')
+return (haystack);
+}
+return(0);
+}
