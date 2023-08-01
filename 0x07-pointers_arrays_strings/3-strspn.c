cf8a2e9398d32ff32936f858d7e8b11d4e42bd43
nancywambui <nwambuimuthee@gmail.com>
2023-08-01 05:46:02 -0700

    3-strspn.c

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strspn - function
+ * @s: a pointer to the input
+ * @accept: its a pointer to the string of the characters
+ * Return: 0 success
+ */
+unsigned int _strspn(char *s, char *accept)
+unsigned int n = 0;
+int r;
+
+while (*s)
+{
+for (r = 0; accept[r]; r++)
+{
+if (*s == accept[r])
+{
+n++;
+break;
+}
+else if (accept[r + 1] == '\0')
+return (n);
+}
+s++;
+}
+return(n);
+}
+
