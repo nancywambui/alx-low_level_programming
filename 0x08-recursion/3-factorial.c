2843c5480f7f3b41ac7f2b764bf242141fa6d2cc
nancywambui <nwambuimuthee@gmail.com>
2023-08-03 13:19:49 -0700

    3-factorial.c

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * factorial - returns fuctorial
+ * @n: number
+ * Return: factorial
+ */
+int factorial(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0)
+		return (1);
+	return (n * factorial(n - 1));
+}
