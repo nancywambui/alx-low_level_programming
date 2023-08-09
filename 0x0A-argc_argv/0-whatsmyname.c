1a0a83e6edd9aab2034fd4871b7a94fb4ffeb4d1
nancywambui <nwambuimuthee@gmail.com>
2023-08-09 00:46:13 -0700

    0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - function printing the name
+ * @argc: input value
+ * @argv: array
+ *
+ * Return: 0 success
+ */
+int main(int argc __attribute__((unused)), char *argv[])
+{
+printf("%s\n", *argv);
+return (0);
+}
