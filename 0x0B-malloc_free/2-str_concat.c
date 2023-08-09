6f34f1e90d083621328acc674f5ceb3b4b6f063b
nancywambui <nwambuimuthee@gmail.com>
2023-08-09 09:27:35 -0700

    2-str_concat.c

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,26 +1,29 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
  * str_concat - function to concatinate
- * @s2: input number
- * Return: concatinate two numbers
+ * @s1: first number
+ * @s2: second number
+ * Return: concatinate s1 and s2
  */
-
 char *str_concat(char *s1, char *s2)
 {
+
 char *conct;
 int i, ci;
+
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
 2 = "";
+
 i = ci = 0;
 while (s1[i] != '\0')
 i++;
 while (s2[ci] != '\0')
 ci++;
 conct = malloc(sizeof(char) * (i + ci + 1));
+
 if (conct == NULL)
 return (NULL);
 i = ci = 0;
@@ -29,6 +32,7 @@ while (s1[i] != '\0')
 conct[i] = s1[i];
 i++;
 }
+
 while (s2[ci] != '\0')
 {
 conct[i] = s2[ci];
